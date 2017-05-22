8e2e37cda78eece7957df736128cc6e91035b58b
Jorge <georgevazj@gmail.com>
2017-05-22 12:16:12 +0200

    Inicio del proyecto

---
+++ src/main/java/com/bbva/Launcher.java
@@ -0,0 +1,17 @@
+package com.bbva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jorge on 22/05/17.
+ */
+
+@SpringBootApplication
+public class Launcher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Launcher.class, args);
+    }
+
+}
